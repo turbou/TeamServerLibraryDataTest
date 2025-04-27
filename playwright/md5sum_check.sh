7d3711b29435c739e94a2ec30aef6a19e9c234d4
Taka <taka.shiozaki@contrastsecurity.com>
2025-04-27 22:39:01 +0900

    update.

--- playwright/md5sum_check.sh
+++ playwright/md5sum_check.sh
@@ -4,16 +4,16 @@ DIRECTORY="/opt/contrast/work"
 CHECK_INTERVAL=60
 TIMEOUT=600
 
-MD5SUM_FILE=`find . -maxdepth 1 -name "*.zip-md5.txt"`
+MD5SUM_FILE=`find $DIRECTORY -maxdepth 1 -name "*.zip-md5.txt"`
 if [ -z "$MD5SUM_FILE" ]; then
   echo "エラー: MD5SUMファイル '$DIRECTORY/$MD5SUM_FILE' が存在しません。"
   exit 1
 fi
 
-base_name=`basename $MD5SUM_FILE .zip-md5.txt``
+base_name=`basename $MD5SUM_FILE .zip-md5.txt`
 ZIP_FILE="${DIRECTORY}/${base_name}.zip"
 
-EXPECTED_MD5SUM=`cat "$DIRECTORY/$MD5SUM_FILE"`
+EXPECTED_MD5SUM=`cat "$MD5SUM_FILE"`
 if [ -z "$EXPECTED_MD5SUM" ]; then
   echo "エラー: MD5SUMファイル '$DIRECTORY/$MD5SUM_FILE' が空です。"
   exit 1
@@ -28,7 +28,7 @@ while true; do
   TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
   if [ -f "$ZIP_FILE" ]; then
     echo "$TIMESTAMP: ZIPファイル '$ZIP_FILE' が存在します。MD5SUMを計算します..."
-    ACTUAL_MD5SUM=`md5sum "$ZIP_FILE"`
+    ACTUAL_MD5SUM=`md5sum "$ZIP_FILE" | awk '{print $1}'`
     echo "$TIMESTAMP: ZIPファイルのMD5SUM: $ACTUAL_MD5SUM"
     if [ "$EXPECTED_MD5SUM" == "$ACTUAL_MD5SUM" ]; then
       echo "$TIMESTAMP: MD5SUMが一致しました！"
@@ -40,14 +40,12 @@ while true; do
     echo "$TIMESTAMP: ZIPファイル '$ZIP_FILE' はまだ存在しません。待機します..."
   fi
 
-  # タイムアウトチェック
   CURRENT_TIME=`date +%s`
   ELAPSED_TIME=`expr "$CURRENT_TIME" - "$START_TIME"`
   if [ "$ELAPSED_TIME" -ge "$TIMEOUT" ]; then
     echo "Timeout reached (${TIMEOUT} seconds). Exiting."
     exit 1
   fi
-
   echo "Waiting for ${CHECK_INTERVAL} seconds..."
   sleep "$CHECK_INTERVAL"
 done

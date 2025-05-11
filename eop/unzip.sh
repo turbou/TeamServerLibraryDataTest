24eebb13611de1b067dfbbfd94aa3d7cedb02397
Taka Shiozaki <taka.shiozaki@contrastsecurity.com>
2025-05-11 09:55:56 +0900

    update.

--- eop/unzip.sh
+++ eop/unzip.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-docker-compose -p eop exec teamserver bash -c "cd /work && unzip Contrast-Data-Export-*.zip -d /opt/contrast/data/libraries/"
+ZIP_FILENAME=$1
+docker-compose -p eop exec teamserver bash -c "cd /work && unzip ${ZIP_FILENAME} -d /opt/contrast/data/libraries/"
 
 exit 0
 

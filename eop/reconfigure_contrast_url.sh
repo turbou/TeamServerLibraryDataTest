6da2733d67521a0406160ac74dc6ce0d56ecf215
Taka Shiozaki <taka.shiozaki@contrastsecurity.com>
2025-05-25 00:07:09 +0900

    update.

---
+++ eop/reconfigure_contrast_url.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "引数でEIPが渡されていません。"
+  exit 1
+fi
+EIP=$1
+echo "teamserver.url=http://${EIP}/Contrast を /opt/contrast/data/conf/general.properties に追記します。"
+docker-compose -p eop exec teamserver sh -c "echo \"teamserver.url=http://${EIP}/Contrast\" >> /opt/contrast/data/conf/general.properties"
+if [ $? -eq 0 ]; then
+  echo "設定への追記が正常に完了しました。"
+else
+  echo "設定への追記が失敗しました。"
+  exit 1
+fi
+
+exit 0
+
